7459ca401509a2b63f2d863caec2a8b7e731623e
lahari998 <124771313+lahari998@users.noreply.github.com>
2023-05-18 02:35:23 +0530

    Add files via upload

--- software assignment/playlist.py
+++ software assignment/playlist.py
@@ -2,7 +2,7 @@ import os
 import random
 from playsound import playsound
 
-folder_path = '/home/lahari/playlists' 
+folder_path = '/home/lahari/playlists'  
 
 # List all files in the folder
 files = os.listdir(folder_path)
@@ -14,7 +14,7 @@ random.shuffle(files)
 for file_name in files:
     file_path = os.path.join(folder_path, file_name)
     absolute_path = os.path.abspath(file_path)
-
+    print(absolute_path)
     # Play the sound file
     playsound(absolute_path)
 
